5c018f65abaa49644295a9d57ef0a82670a90cd5
Ayoub Outrgua <74433630+Ayoub-Outrgua@users.noreply.github.com>
2025-06-28 23:44:45 +0000

    go reloaded

--- main.go
+++ main.go
@@ -3,62 +3,38 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"goreload/functions"
 )
 
 func main() {
-	// if len(os.Args) != 3 {
-	// 	fmt.Println("the args are not exist!")
-	// 	return
-	// }
+	if len(os.Args) != 3 {
+		fmt.Println("the args are not exist!")
+		return
+	}
 
-	// inputFile := os.Args[1]
-	// outputFile := os.Args[2]
-	inputFile := "sample.txt"
-	outputFile := "result.txt"
-	// if !strings.HasSuffix(outputFile, ".txt") || !strings.HasSuffix(inputFile, ".txt") {
-	// 	fmt.Println("hdkfj")
-	// 	return
-	// }
+	inputFile := os.Args[1]
+	outputFile := os.Args[2]
+
+	if !strings.HasSuffix(outputFile, ".txt") || !strings.HasSuffix(inputFile, ".txt") {
+		fmt.Println("one of the args not correct!!!")
+		return
+	}
 
 	file, err := os.ReadFile(inputFile)
 	if err != nil {
-		fmt.Println("error in read file")
+		fmt.Println("Error in read file ", err)
 		return
 	}
 
-	// slice := strings.Fields(string(file))
-	// fmt.Println(string(file))
-	// fmt.Println(slice)
 	str := functions.ApplayModifications(string(file))
-	// slice = functions.CLeanSlice(slice)
-	// fmt.Println(slice)
-	// combined := strings.Join(slice, " ")
+
 	byteSlice := []byte(str)
 	erre := os.WriteFile(outputFile, byteSlice, 0o644)
 	if erre != nil {
-		fmt.Println("error in write file")
+		fmt.Println("Error in write file ", erre)
 		return
 	}
 
-	// slice = functions.ApplayModifications(slice)
-	// slc1 := strings.Join(functions.ApplayModifications(slice), " ")
-	// slc2 := strings.Join(functions.ApplayModifications(slice), " ")
-
-	// for slc1 != slc2 {
-	// 	slice = functions.ApplayModifications(slice)
-	// 	slc1 = strings.Join(functions.ApplayModifications(slice), " ")
-	// 	slc2 = strings.Join(functions.ApplayModifications(slice), " ")
-	// }
-
-	// slice = functions.CLeanSlice(slice)
-	// fmt.Println(slice)
-	// combined := strings.Join(slice, " ")
-	// byteSlice := []byte(combined)
-	// erre := os.WriteFile(outputFile, byteSlice, 0o644)
-	// if erre != nil {
-	// 	fmt.Println("error in write file")
-	// 	return
-	// }
 }
