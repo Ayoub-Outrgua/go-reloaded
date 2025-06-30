5b2d3d07331380550ba44d6a88b8040e7cae5dd2
Ayoub-Outrgua <outrgua1949@gmail.com>
2025-06-30 17:52:23 +0100

    go-reloaded

--- functions/applayVowels.go
+++ functions/applayVowels.go
@@ -11,9 +11,6 @@ func ApplayVowels(slice []string) []string {
 	str := strings.Join(slice, " ")
 	newStr := ""
 	for i, v := range str {
-		if condition {
-			
-		}
 		if i+2 < len(str) && i+1 < len(str) && i-1 >= 0 && (v == 'a' || v == 'A') &&
 			IsVowel(rune(str[i+2])) && str[i+1] == ' ' && str[i-1] == ' ' {
 			newStr += string(v) + "n"
